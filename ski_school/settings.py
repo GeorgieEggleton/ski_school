6b30f331a8f6421ff3fc5acf434580085b87fc70
GeorgieEggleton <georgie@sailingfaster.co.uk>
2023-12-09 11:51:24 +0000

    Add favicon and styling for Lesson details page

--- ski_school/settings.py
+++ ski_school/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) #user added no staic root setting due to AWS link?
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) #user added no static root setting due to AWS link?
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
