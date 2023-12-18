36a7743549e68a09ecdc60775241a7ff172c6a73
GeorgieEggleton <georgie@sailingfaster.co.uk>
2023-12-18 14:53:50 +0000

    Removed secret key and set debug

--- ski_school/settings.py
+++ ski_school/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$@j^du@lbdi3kd(@sqab$+wmlw76_*)3!-2qvj@l6v)+47h8g='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['alps-snow-school.heroku.com', 'localhost', 'alps-snow-school-4dd405a355b2.herokuapp.com']
 
